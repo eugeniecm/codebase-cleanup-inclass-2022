0ec757e62b62ab87dd11475ab5cefc8ed5e08fb1
eugeniecm <97541454+eugeniecm@users.noreply.github.com>
2022-03-28 18:16:40 -0400

    others

--- app/utils.py
+++ app/utils.py
@@ -12,3 +12,17 @@ def to_usd(my_price):
     Invoke like this: to_usd(9.9999)
     """
     return '${:,.2f}'.format(my_price)
+
+
+#if this code is in the global scope of a file wer're trying to import from 
+#... it will throw errors when we try to run these
+#price = input("Please chosse a price like 4.9999")
+#print(to_usd(float(price)))
+
+if __name__ == "__main__":
+    #nesting code in the main condition will allow other scritps to clearly
+    #import function from this file without 
+    #if this code is in the global scope of a file wer're trying to import from 
+    #... it will throw errors when we try to run these
+    price = input("Please choose a price like 4.9999")
+    print(to_usd(float(price)))
